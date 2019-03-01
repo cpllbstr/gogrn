4682e3cf202f2f13accfc228cab20020140aa76b
Golubitskiy Savva <golubitskiy_savv@mail.ru>
2019-03-02 00:51:49 +0300

    rk4 for matrix completed

---
+++ ode/envs/mathenvs.go
@@ -0,0 +1,79 @@
+package envs
+
+import (
+	"gonum.org/v1/gonum/mat"
+)
+
+//Float64Env - predefined MathEnv for float64 type
+var Float64Env float64Env
+
+type float64Env struct {
+}
+
+func (e float64Env) MultFloat(f float64, x interface{}) interface{} {
+	return f * x.(float64)
+}
+
+func (e float64Env) SubFloat(x interface{}, f float64) interface{} {
+	return x.(float64) / f
+}
+
+func (e float64Env) Summ(args ...interface{}) interface{} {
+	summ := 0.
+	for i := 0; i < len(args); i++ {
+		summ += args[i].(float64)
+	}
+	return summ
+}
+
+//Complex128Env - predefined MathEnv for complex128 type
+var Complex128Env complex128Env
+
+type complex128Env struct {
+}
+
+func (e complex128Env) MultFloat(f float64, x interface{}) interface{} {
+	return complex(f, 0) * x.(complex128)
+}
+
+func (e complex128Env) SubFloat(x interface{}, f float64) interface{} {
+	return x.(complex128) / complex(f, 0)
+}
+
+func (e complex128Env) Summ(args ...interface{}) interface{} {
+	summ := complex(0., 0.)
+	for i := 0; i < len(args); i++ {
+		summ += args[i].(complex128)
+	}
+	return summ
+}
+
+//GonumVecDenseEnv - predefined MathEnv for gonum DenseVec  type
+var GonumVecDenseEnv gonumDenseEnv
+
+type gonumDenseEnv struct {
+}
+
+func (e gonumDenseEnv) MultFloat(f float64, x interface{}) interface{} {
+	vec := x.(*mat.VecDense)
+	res := mat.NewVecDense(vec.Len(), nil)
+	res.ScaleVec(f, vec)
+	return res
+}
+
+func (e gonumDenseEnv) SubFloat(x interface{}, f float64) interface{} {
+	vec := x.(*mat.VecDense)
+	res := mat.NewVecDense(vec.Len(), nil)
+	scale := 1 / f
+	res.ScaleVec(scale, vec)
+	return res
+}
+
+func (e gonumDenseEnv) Summ(args ...interface{}) interface{} {
+	summ := mat.NewVecDense(args[0].(*mat.VecDense).Len(), nil)
+	for i := 0; i < len(args); i++ {
+		x := args[i].(*mat.VecDense)
+		summ.AddVec(summ, x)
+	}
+	return summ
+}
--- ode/rk4.go
+++ ode/rk4.go
@@ -1,26 +1,60 @@
 package ode
 
-import (
-	"gonum.org/v1/gonum/mat"
-)
+//Func2Var - type of function with 2 variables
+/*
+	Example:
 
-type RK4 struct {
-	y0 interface{}
-    interface{}
+	func plus(x interface{}, y interface{}) interface{} {
+		return x.(float64) + y.(float64)
+	}
+*/
+type Func2Var func(interface{}, interface{}) interface{}
+
+//Rk4Func - desccribes how rk4 should look
+/*
+	Rk4 for any type of y
+	!You should define MathEnviroment for your type of data
+
+	rk4(y0 interface, time, step float64, func Func2Var)
+
+	y(t_0) = y_0 - Border conditions
+ 	dy/dt = f(t, y) - equation
+ 	t - start time
+	h - time step
+*/
+type Rk4Func func(interface{}, float64, float64, Func2Var) interface{}
+
+//MathEnv - describes how to operate with your type of data
+/*
+	X - your type of data
+	MultFloat(float64, interface{}) interface{}
+	returns X as float*X
+	SubFloat(interface{}, float64) interface{}
+	returns X as X/float
+	Summ(...interface{}) interface{}
+	returns X as Summ of args
+*/
+type MathEnv interface {
+	MultFloat(float64, interface{}) interface{}
+	Summ(...interface{}) interface{}
+	SubFloat(interface{}, float64) interface{}
 }
 
-//Rk4 - Runge-Kutta4
+//Rk4FromEnv - Runge-Kutta4 for any type of input data
 /*
- y(t_0) = y_0 - Border conditions
- dy/dt = f(t, y) - equation
- t - start time
- h - step
-*//*
-func Rk4(yi, t, h float64, f func(interface{}, interface{}) interface{}) {
-	k_1 := h * f(t_n, y_n)
-	k_2 := h * f(t_n+h/2., y_n+k_1/2.)
-	k_3 := h * f(t_n+h/2., y_n+k_2/2.)
-	k_4 := h * f(t_n+h, y_n+k_3)
-
-	return y_n + 1/6.*(k_1+2*k_2+2*k_3+k_4)
-}*/
+	Rk4 for any type of y
+	You should define MathEnviroment for your type of data
+	y(t_0) = y_0 - Border conditions
+ 	dy/dt = f(t, y) - equation
+ 	t - start time
+	h - time step
+*/
+func Rk4FromEnv(env MathEnv) Rk4Func {
+	return func(y interface{}, t, h float64, f Func2Var) interface{} {
+		k1 := env.MultFloat(h, f(t, y))                                     //k_1 = h*f(t,y)
+		k2 := env.MultFloat(h, f(t+h/2., env.Summ(y, env.SubFloat(k1, 2)))) //k_2 = h*f(t+h/2,y+k_1/2)
+		k3 := env.MultFloat(h, f(t+h/2., env.Summ(y, env.SubFloat(k2, 2)))) //k_3 = h*f(t+h/2,y+k_2/2)
+		k4 := env.MultFloat(h, f(t+h, env.Summ(y, k3)))                     //k_2 = h*f(t+h,y+k_3)
+		return env.Summ(y, env.SubFloat(env.Summ(k1, env.MultFloat(2, k2), env.MultFloat(2, k3), k4), 6))
+	}
+}
