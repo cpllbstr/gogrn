38f68fab9d2be50d83096b7b46abd57392ff4bca
Golubitskiy Savva <golubitskiy_savv@mail.ru>
2019-03-02 00:37:53 +0300

    rk4 for matrix completed

--- grn/params.go
+++ grn/params.go
@@ -4,28 +4,27 @@ import (
 	"gonum.org/v1/gonum/mat"
 )
 
-//Body - Body for Math model
-type Body struct {
+//ThreeBodyModel - Body for Math model
+type ThreeBodyModel struct {
 	K [3]float64
 	M [3]float64
-	L float64
-}
-
-//GenMatr - generates matrix for setted Body
-func (c Body) GenMatr() *mat.Dense {
-	data := []float64{
-		0., 0., 0., 1., 0., 0.,
-		0., 0., 0., 0., 1., 0.,
-		0., 0., 0., 0., 0., 1.,
-		-((c.K[0] + c.K[1]) / c.M[0]), c.K[1] / c.M[0], 0, 0, 0, 0,
-		c.K[1] / c.M[1], -((c.K[1] + c.K[2]) / c.M[1]), c.K[2] / c.M[1], 0, 0, 0,
-		0, c.K[2] / c.M[2], c.K[2] / c.M[2], 0, 0, 0,
-	}
-	return mat.NewDense(6, 6, data)
 }
 
 //Condition - describes condition of 3 body model for time t
 type Condition struct {
 	V [3]float64
 	X [3]float64
+}
+
+//GenMatr - generates matrix for setted Body
+func (c ThreeBodyModel) GenMatr() *mat.Dense {
+	data := []float64{
+		0., 0., 0., 1., 0., 0.,
+		0., 0., 0., 0., 1., 0.,
+		0., 0., 0., 0., 0., 1.,
+		-((c.K[0] + c.K[1]) / c.M[0]), c.K[1] / c.M[0], 0, 0, 0, 0,
+		c.K[1] / c.M[1], -((c.K[1] + c.K[2]) / c.M[1]), c.K[2] / c.M[1], 0, 0, 0,
+		0, c.K[2] / c.M[2], -c.K[2] / c.M[2], 0, 0, 0,
+	}
+	return mat.NewDense(6, 6, data)
 }
--- main.go
+++ main.go
@@ -3,80 +3,46 @@ package main
 import (
 	"fmt"
 
+	"github.com/cpllbstr/gogrn/grn"
+	"github.com/cpllbstr/gogrn/ode/envs"
+
+	"github.com/cpllbstr/gogrn/ode"
+
 	"gonum.org/v1/gonum/mat"
 )
 
-type ff func(param, param) interface{}
-
-type function struct {
-	fun ff
-}
-type param struct {
-	val interface{}
-}
-
-func Call(p1 param, p2 param, f function) interface{} {
-	return f.fun(p1, p2)
-}
-
-/*
-func call(f fu, param1, param2 interface{}) interface{} {
-	res := f(param1, param2)
-	return res
-}*/
-
-func plus(x param, y param) interface{} {
-	return x.val.(float64) + y.val.(float64)
-}
-func dot(x param, y param) interface{} {
-	m := x.val.(mat.Matrix)
-	v := y.val.(mat.Vector)
-	var res mat.VecDense
-	res.MulVec(m, v)
-	return res
+func CreateDot(A *mat.Dense) ode.Func2Var {
+	return func(x interface{}, y interface{}) interface{} {
+		vec := y.(*mat.VecDense)
+		res := mat.NewVecDense(vec.Len(), nil)
+		res.MulVec(A, vec)
+		return res
+	}
 }
 
 func main() {
-	/*
-		contactC := grn.Body{
-			K: [3]float64{1.5, 1.5, 1.5},
-			M: [3]float64{1, 1, 1},
-			L: 1.5,
-		}
+	contactC := grn.ThreeBodyModel{
+		K: [3]float64{1.5, 1.5, 1.5},
+		M: [3]float64{1, 1, 1},
+	}
+	/*freeC := grn.ThreeBodyModel{
+		K: [3]float64{0, 1.5, 1.5},
+		M: [3]float64{1, 1, 1},
+	}*/
+	//free := freeC.GenMatr()
+	contact := contactC.GenMatr()
+	//fmt.Printf("% v\n", mat.Formatted(contact))
+	vec := mat.NewVecDense(6, []float64{0, 0, 0, -1, -1, -1})
+	//fmt.Printf("% v\n%v\n", mat.Formatted(free), mat.Formatted(vec))
+	//A := mat.NewDense(2, 2, []float64{5, 4, 4, 5})
+	//Y0 := mat.NewVecDense(2, []float64{2, 0})
+	dot := CreateDot(contact)
+	rk4 := ode.Rk4FromEnv(envs.GonumVecDenseEnv)
+	res := rk4(vec, 0., 0.02, dot)
+	/*for i := 0; i < 100; i++ {
+		res = rk4(res, float64(i)*0.01, 0.01, dot)
+	}*/
 
-		freeC := grn.Body{
-			K: [3]float64{0, 1.5, 1.5},
-			M: [3]float64{1, 1, 1},
-			L: 1.5,
-		}
-			free := freeC.GenMatr()
-			contact := contactC.GenMatr()
-			fmt.Printf("% v\n", mat.Formatted(contact))
-			vec := mat.NewVecDense(6, []float64{0, 0, 0, 1, 1, 1})
-			fmt.Printf("% v\n%v\n", mat.Formatted(free), mat.Formatted(vec))
-	*/
-	matr := mat.NewDense(2, 2, []float64{1, 2, 3, 4})
-	vec1 := mat.NewVecDense(2, []float64{1, 2})
-	fmt.Printf("% v\n%v\n\n", mat.Formatted(matr), mat.Formatted(vec1))
+	fmt.Println(mat.Formatted(res.(*mat.VecDense)))
 
-	x := param{val: matr}
-	y := param{val: vec1}
-	f := function{fun: dot}
-	res := Call(x, y, f)
-
-	out := res.(mat.VecDense)
-	fmt.Println(mat.Formatted(&out))
-
-	/*
-		x := param{val: 10.}
-		y := param{val: 20.}
-		f := function{fun: plus}
-		x = param{val: contact}
-		y = param{val: vec}
-		f = function{fun: dot}
-		res := Call(x, y, f)
-
-		out := res.(mat.VecDense)
-		fmt.Println(mat.Formatted(&out))
-	*/
 }
